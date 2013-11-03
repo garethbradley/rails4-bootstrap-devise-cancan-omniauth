295d917415103312c869fe5adf30c3cc5870f502
Gareth Bradley <gb@garethbradley.co.uk>
2013-11-03 17:48:08 +0000

    Update rails4-bootstrap.rb

--- rails4-bootstrap.rb
+++ rails4-bootstrap.rb
@@ -84,8 +84,8 @@ gsub_file 'config/routes.rb', 'devise_for :users', ''
 migration_devise_user = Dir.glob(File.join("**", "db", "migrate", "[0-9]{14}_devise_create_users.rb")).first
 migration_identities = Dir.glob(File.join("**", "db", "migrate", "[0-9]{14}_create_identities.rb")).first
 
-raise migration_devise_user.inspect.to_s
-puts migration_identities
+raise migration_devise_user.to_s
+puts migration_identities.to_s
 
 ### Routes
 route 'resources :users'
