e049b14ba88a74d1441bb2122eeb2f0a7f624371
Gareth Bradley <gb@garethbradley.co.uk>
2013-11-03 08:49:31 +0000

    Update user.rb

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,14 +2,13 @@ class User < ActiveRecord::Base
   include User::AuthDefinitions
   include User::Roles
 
+  # Include default devise modules. Others available are:
+  # :token_authenticatable, :confirmable,
+  # :lockable, :timeoutable and :omniauthable
+  devise :database_authenticatable, :registerable,
+         :recoverable, :rememberable, :trackable, :validatable
+
   has_many :identities
-
-
-  #field :email, type: String
-  #field :image, type: String
-  #field :first_name, type: String
-  #field :last_name, type: String
-  #field :roles_mask, type: Integer
   
   validates_presence_of :email, :first_name, :last_name
 
